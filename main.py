1575edddc7272ee6eed41e05430cceefb74a70a1
alexgiving <alexgiving@mail.ru>
2023-01-05 15:47:46 +0300

    add notebooks

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
+from sklearn.datasets import make_blobs
 
 from lib.lk_norm import lk_norm
 
 
 def main():
 
-    X = np.random.random_sample(10)
-    Y = np.random.random_sample(10)
+    (X, Y), labels = make_blobs(n_samples=2, centers=2, random_state=42)
 
     res = lk_norm(X, Y, p=0.1)
     print(res)
