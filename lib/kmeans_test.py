decccf3ebdea95302d953123bd9f5af4663ec6da
alexgiving <alexgiving@mail.ru>
2023-01-07 17:53:30 +0300

    new tests

--- lib/kmeans_test.py
+++ lib/kmeans_test.py
@@ -1,11 +1,15 @@
 """Test KMeans."""
 import numpy as np
 import pytest
+import seaborn as sns
 from matplotlib import pyplot as plt
+from sklearn.datasets import make_blobs
 
+from lib.deprecated import deprecated
 from lib.kmeans import KMeans
 
 
+@deprecated
 def generate_random_centers(start, stop, k):
     """Generate k random centers."""
     centers = []
@@ -14,6 +18,7 @@ def generate_random_centers(start, stop, k):
     return centers
 
 
+@deprecated
 def generate_random_data_by_centers(n_dots, centers):
     """Generate data."""
     data = []
@@ -22,7 +27,7 @@ def generate_random_data_by_centers(n_dots, centers):
         data.append(new_sample)
     return np.concatenate(data, axis = 0)
 
-
+@deprecated
 def plot(data, centers, name_postfix=""):
     """Plot data and centers."""
     plt.scatter(data[:,0], data[:,1], s=7)
@@ -34,13 +39,30 @@ def plot(data, centers, name_postfix=""):
     plt.close()
 
 
-@pytest.mark.parametrize("k", [2, 3, 4])
-@pytest.mark.parametrize("parameter", [2, 0.1, 0.01])
-def test_kmeans(k, parameter):
-    """Test KMeans."""
-    centres = generate_random_centers(0, 10, k)
-    data = generate_random_data_by_centers(200, centres)
+def plot_colored(data, centers, labels, name_postfix=""):
+    """Plot data and centers."""
+    sns.scatterplot(x=data[:,0],
+                    y=data[:,1],
+                    hue=labels,
+                    palette="deep",
+                    legend=None
+                    )
+    plt.plot(centers[:,0], centers[:,1], 'k+', markersize=10)
+    if name_postfix:
+        plt.savefig(f'images/myfig_{name_postfix}.png')
+    else:
+        plt.savefig('images/myfig.png')
+    plt.close()
 
-    kmeans = KMeans(k=k, parameter=parameter)
-    centers = kmeans.fit(data)
-    plot(data, centers, name_postfix=k)
+
+@pytest.mark.parametrize("k", [2, 3, 4])
+@pytest.mark.parametrize("parameters", [[0.01, 0.1, 2]])
+def test_kmeans(k, parameters):
+    """Test KMeans."""
+    data, labels, *_ = make_blobs(n_samples=200, centers=k)
+
+    for parameter in parameters:
+        kmeans = KMeans(k=k, parameter=parameter)
+        centers = kmeans.fit(data)
+        plot_colored(data, centers, labels, name_postfix=f"{k}_{parameter}")
+        print('creating gif\n')
