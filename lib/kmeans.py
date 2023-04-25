79a43f78504b32f99b424a84cd6510f6cc355017
alexgiving <alexgiving@mail.ru>
2023-04-26 00:16:45 +0300

    fix labling

--- lib/kmeans.py
+++ lib/kmeans.py
@@ -7,39 +7,44 @@ from lib.types import p_type
 
 
 class KMeans:
-    def __init__(self, n_clusters: int, max_iter: int = 1, p: p_type = 2):
+    def __init__(self, n_clusters: int, max_iter: int = 10, p: p_type = 2):
         self.n_clusters = n_clusters
         self.max_iter = max_iter
         self.p = p
-        self.centroids = []
-        self.labels = []
+
+    @staticmethod
+    def assign_to_cluster(X: np.ndarray, centroids: np.ndarray, n_clusters: int, p: p_type):
+        clusters = [[] for _ in range(n_clusters)]
+        labels = []
+
+        # assign each data point to the closest centroid
+        for point in X:
+            distances_to_each_cebtroid = pairwise_minkowski_distance(
+                point, centroids, p)
+
+            closest_centroid = np.argmin(distances_to_each_cebtroid)
+            clusters[closest_centroid].append(point)
+            labels.append(closest_centroid)
+        return clusters, labels
 
     def fit(self, X: np.ndarray):
         # initialize centroids randomly
-        self.centroids = [X[i] for i in np.random.choice(
-            X.shape[0], self.n_clusters, replace=False)]
+
+        indices = np.random.choice(X.shape[0], self.n_clusters, replace=False)
+        self.centroids = X[indices]
 
         for _ in range(self.max_iter):
-            clusters = [[] for _ in range(self.n_clusters)]
-            self.labels = []
-
-            # assign each data point to the closest centroid
-            for point in X:
-                distances_to_each_cebtroid = pairwise_minkowski_distance(
-                    point, self.centroids, self.p)
-
-                closest_centroid = np.argmin(distances_to_each_cebtroid)
-                clusters[closest_centroid].append(point)
-                self.labels.append(closest_centroid)
+            clusters, self.labels = self.assign_to_cluster(
+                X, self.centroids, self.n_clusters, self.p)
 
             # update centroids using the specified optimizer
             for cluster_id, cluster in enumerate(clusters):
                 cluster = np.array(cluster)
-                data_dimension = cluster.shape[0]
+                data_dimension = cluster.shape[1]
                 new_centroid = np.array([])
 
                 for coordinate_id in range(data_dimension):
-                    dimension_slice = cluster[coordinate_id, :]
+                    dimension_slice = cluster[:, coordinate_id]
 
                     if self.p == 2:
                         value = mean_optimizer(dimension_slice)
@@ -55,4 +60,8 @@ class KMeans:
                     # elif self.optimizer in ('Powell', 'CG', 'BFGS', 'L-BFGS-B', 'TNC', 'COBYLA', 'trust-constr'):
                     #     self.centroids[j] = minimize(lambda x: minkowski_distance(x, cluster, self.p), self.centroids[j], method=self.optimizer).x.copy()
                 self.centroids[cluster_id] = new_centroid
+
+        _, self.labels = self.assign_to_cluster(
+            X, self.centroids, self.n_clusters, self.p)
+
         return self.centroids, self.labels
