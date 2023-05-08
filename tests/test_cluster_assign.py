d7c76799f8c02dcaa5b7f0984e17ab2f0792de6a
alexgiving <alexgiving@mail.ru>
2023-05-08 20:59:12 +0300

    fix lint

--- tests/test_cluster_assign.py
+++ tests/test_cluster_assign.py
@@ -2,7 +2,6 @@ import numpy as np
 import pytest
 
 from lib.kmeans import assign_to_cluster
-from lib.types import p_type
 
 
 def _get_test_params():
@@ -12,20 +11,20 @@ def _get_test_params():
                 [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
                 [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
             ), np.array([[0, 0], [1, 1]]),
-            [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+            np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
         ),
         (
             np.array([
                 [0.500001, 0.50000001], [0, 0], [0, 0], [0, 0], [0, 0],
                 [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]
             ), np.array([[0, 0], [1, 1]]),
-            [1, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+            np.array([1, 0, 0, 0, 0, 1, 1, 1, 1, 1])
         )
     ]
     return params
 
 
-def _get_test_p():
+def _get_test_p() -> list[float | int]:
     p = [0.2, 0.5, 1, 2]
     return p
 
@@ -33,6 +32,9 @@ def _get_test_p():
 @pytest.mark.kmeans
 @pytest.mark.parametrize("test_input, centroids, expected_output", _get_test_params())
 @pytest.mark.parametrize("p", _get_test_p())
-def test_assign_to_cluster(test_input, centroids, expected_output, p: p_type):
+def test_assign_to_cluster(test_input: np.ndarray,
+                           centroids: np.ndarray,
+                           expected_output: np.ndarray,
+                           p: float | int) -> None:
     _, label = assign_to_cluster(test_input, centroids, len(centroids), p)
     np.testing.assert_array_equal(expected_output, label)
