a0339bffa6b6173096e6b99a25c375386c291985
Trutnev Aleksei <alexgiving@mail.ru>
2024-05-01 23:20:08 +0300

    rm

--- lkmeans/clustering/__init__.py
+++ lkmeans/clustering/__init__.py
@@ -1,3 +1,2 @@
-from lkmeans.clustering.base import assign_to_cluster
-from lkmeans.clustering.semi_supervised import SoftSSLKMeans
+from lkmeans.clustering.semi_supervised import SoftSSLKMeans, HardSSLKMeans
 from lkmeans.clustering.unsupervised import LKMeans
