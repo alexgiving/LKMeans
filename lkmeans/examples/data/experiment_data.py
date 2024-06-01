23fdaae14e75591463933cd70b67eb8b65264365
Trutnev Aleksei <alexgiving@mail.ru>
2024-06-01 17:58:14 +0300

    [MERGE] Introduced new experiment generator
    [MERGE] Introduced new experiment generator

--- lkmeans/examples/data/experiment_data.py
+++ lkmeans/examples/data/experiment_data.py
@@ -1,7 +1,9 @@
 import numpy as np
 from numpy.typing import NDArray
 
-from lkmeans.examples.experiment import get_covariance_matrix
+
+def get_covariance_matrix(sigma: float, dimension: int) -> NDArray:
+    return np.eye(dimension) * sigma
 
 
 def get_experiment_data(num_clusters: int, dimension: int) -> tuple[int, float, list[NDArray], list[NDArray]]:
@@ -9,21 +11,16 @@ def get_experiment_data(num_clusters: int, dimension: int) -> tuple[int, float, 
     Function for generation the synthetic data for experiments
     by dimension and number of clusters
     '''
-    n_clusters: int = 0
     prob: float = 0.
     mu_prefix: list[list[float | int]] = [[]]
     sigma_list: list[float | int] = []
 
     if num_clusters == 2:
-        print('Experiment with 2 clusters')
-        n_clusters = 2
         sigma_list = [1, 1]
         prob = 0.5
         mu_prefix = [[-4, 0], [4, 0]]
 
     elif num_clusters == 3:
-        print('Experiment with 3 clusters')
-        n_clusters = 3
         sigma_list = [1, 1, 1]
         prob = 1/3
         mu_prefix = [[4, 0, 0], [0, 4, 0], [0, 0, 4]]
@@ -33,4 +30,4 @@ def get_experiment_data(num_clusters: int, dimension: int) -> tuple[int, float, 
 
     mu_list = [np.array([x + [0] * (dimension - len(x))]) for x in mu_prefix]
     cov_matrix = [get_covariance_matrix(sigma, dimension) for sigma in sigma_list]
-    return n_clusters, prob, mu_list, cov_matrix
+    return num_clusters, prob, mu_list, cov_matrix
