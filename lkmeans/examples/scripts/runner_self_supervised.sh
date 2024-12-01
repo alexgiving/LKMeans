a427cac242de6ad6fea450c1288c0a9c2659c7ba
Trutnev Aleksei <alexgiving@mail.ru>
2024-12-01 20:13:58 +0300

    logging fix

--- lkmeans/examples/scripts/runner_self_supervised.sh
+++ lkmeans/examples/scripts/runner_self_supervised.sh
@@ -10,7 +10,7 @@ MINKOSKI_VALUES=(0.5 1 2 5)
 T_VALUES=(0 0.2 0.4 0.6 0.8)
 N_POINTS_VALUES=(100 500 1000)
 PREPROCESSOR_VALUES=(pca tsne)
-PREPROCESS_COMPONENTS=(2 5 8 10 15)
+PREPROCESS_COMPONENTS=(2 4 8 16 15)
 DIMENSION_VALUES=(20)
 NUM_CLUSTERS_VALUES=(2 3)
 
@@ -29,7 +29,7 @@ for NUM_CLUSTERS in "${NUM_CLUSTERS_VALUES[@]}";do
                         for PREPROCESS_COMPONENT in "${PREPROCESS_COMPONENTS[@]}";do
 
 
-NAME="${CLUSTERING}_|_self_supervision_${PREPROCESSOR}_|_num-clusters_${NUM_CLUSTERS}_|_minkowski_${MINKOVSKI}_|_t_${T}_|_n-points_${N_POINTS}_|_dimension_${DIMENSION}_|_repeats_${REPEATS}.log"
+NAME="${CLUSTERING}_|_self_supervision_${PREPROCESSOR}_|_self_supervision_n_components_${PREPROCESS_COMPONENT}_|_num-clusters_${NUM_CLUSTERS}_|_minkowski_${MINKOVSKI}_|_t_${T}_|_n-points_${N_POINTS}_|_dimension_${DIMENSION}_|_repeats_${REPEATS}.log"
 
                             echo ${NAME}
                             PARAMETERS="
