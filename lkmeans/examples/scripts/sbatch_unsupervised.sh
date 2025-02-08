b35f860ed0f0b8d62fae633cffcd6ad33e42ff0e
Trutnev <alexgiving@mail.ru>
2025-02-08 15:27:18 +0300

    save

---
+++ lkmeans/examples/scripts/sbatch_unsupervised.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#SBATCH --job-name="unsupervised_clustering"
+#SBATCH --output=%x_%j.out
+
+#SBATCH --gpus=0
+#SBATCH --time=7-0:0
+
+module purge
+module load Python
+
+source deactivate
+source activate lkmeans_venv
+
+# Executable
+export PYTHONPATH=${PYTHONPATH}:$(pwd)
+
+srun bash ./lkmeans/examples/scripts/runner_unsupervised.sh
