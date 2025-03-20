e045965de2c2bce7559fbf85a0e12a3e9dfe43ab
Trutnev <alexgiving@mail.ru>
2025-03-20 19:53:19 +0300

    save

--- report/charts/main.py
+++ report/charts/main.py
@@ -27,6 +27,13 @@ def configure_x_axis(axes: Axes, json_data: Dict[str, Any]) -> Axes:
     return axes
 
 
+def parse_log(parser: LogParser, line: str) -> Dict[str, float]:
+    if len(line.split(' ')) > 1:
+        return json.loads(line.replace('\'', '"'))
+    
+    return parser.parse(line)
+
+
 def main() -> None:
     args = ChartArgumentParser(underscores_to_dashes=True).parse_args()
     with args.config.open() as file:
@@ -37,14 +44,13 @@ def main() -> None:
     parser = LogParser()
     data = defaultdict(list)
     for block_name, logs_block in json_data['logs'].items():
-        if block_name == 'LKMeans':
-            continue
         for log_path in logs_block.values():
-            if len(log_path.split(' ')) > 1:
-                log_data_dict = json.loads(log_path.replace('\'', '"'))
-            else:
-                log_data_dict = parser.parse(log_path)
+            log_data_dict = parse_log(parser, log_path)
             data[block_name].append(log_data_dict)
+
+    baseline_path = json_data.get('baseline', None)
+    if baseline_path:
+        baseline_data_dict = parse_log(parser, baseline_path)
 
     for metric in json_data['plot_metrics']:
         config_name = json_data['name']
@@ -56,6 +62,9 @@ def main() -> None:
 
         for line_name, values in prepared_data.items():
             axes.plot(values, label=line_name)
+        
+        if baseline_data_dict is not None:
+            axes.plot([baseline_data_dict[metric]]*len(values), '--', label="Baseline")
 
         axes.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
                       ncols=2, mode="expand", borderaxespad=0.)
