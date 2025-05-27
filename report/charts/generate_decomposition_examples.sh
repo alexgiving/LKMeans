d49d026bd11334ef76b6d8f9b11bc7fa7e9d06a9
Trutnev <alexgiving@mail.ru>
2025-05-27 12:11:10 +0300

    rm

--- report/charts/generate_decomposition_examples.sh
+++
@@ -1,8 +0,0 @@
-#! bin/bash
-
-python ./report/charts/generate_decomposition_example.py --t-parameter 0.4
-python ./report/charts/generate_decomposition_example.py --t-parameter 0.8
-
-python ./report/charts/generate_decomposition_example.py --dataset wine
-python ./report/charts/generate_decomposition_example.py --dataset breast_cancer
-python ./report/charts/generate_decomposition_example.py --dataset digits
